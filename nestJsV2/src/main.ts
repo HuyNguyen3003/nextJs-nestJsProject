3d06cf03855a89526ce691bd18cc764092962014
HuyNguyen3003 <6251071040@st.utc2.edu.vn>
2023-07-14 22:58:40 +0700

    07 Generating OpenAPI Specification

--- nestJsV2/src/main.ts
+++ nestJsV2/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-err.filter';
 import { ApiKeyGuard } from './common/guards/api-key/api-key.guard';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +15,18 @@ async function bootstrap() {
     }
   }))
   app.useGlobalFilters(new HttpExceptionFilter())
-  app.useGlobalGuards(new ApiKeyGuard())
+  //app.useGlobalGuards(new ApiKeyGuard())
+
+
+  const options = new DocumentBuilder()
+    .setTitle('Iluvcoffee')
+    .setDescription('Coffee application')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
+
+
   await app.listen(3000);
 }
 bootstrap();

a23defdb3e18eb08e06d408c48c5aa0d2b62a06f
HuyNguyen3003 <6251071040@st.utc2.edu.vn>
2023-07-24 17:09:01 +0700

    8. Adding Backend Code with API Routes (Fullstack React)

--- nextjs-v2/pages/_app.js
+++ nextjs-v2/pages/_app.js
@@ -1,17 +1,7 @@
-import Layout from "../components/layout/layout";
-import "../styles/globals.css";
-import Head from "next/head";
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Head>
-        <title>nextJs</title>
-        <meta name="viewport" content="initila-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
